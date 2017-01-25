ee5ee1b16f451b6dc1e066909a65a6a3db1827f5
shbour <shawn.bourgeois2@gmail.com>
2017-01-25 10:19:43 -0500

    Collaborators link to okcash.co

--- sell.php
+++ sell.php
@@ -29,7 +29,8 @@
 						<li><a href="projects">Projects</a></li>
 						<li><a href="buy"  class="active">Trade</a></li>
 						<li><a href="http://okcash.co/faq.html" target="_blank">OKCash FAQ</a></li>
-					</ul>
+			                        <li><a href="http://okcash.co/collaborators.html">OK collaborators</a></li>					
+                                        </ul>
 				</nav>
 			</header>
 
